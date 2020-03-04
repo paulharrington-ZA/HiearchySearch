b226cbef4fcf8bf38d8c4f9e522da07fa919c2a1
info_000 <info_000@DEV001>
2020-03-04 14:09:29 +0200

    Add project files.

---
+++ TextSearchApp/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextSearchApp
+{
+    internal static class StringExtensions
+    {
+        internal static bool IsMatch(this string s, string o, bool matchCase)
+        {
+            var regex = matchCase ? new Regex(o) : new Regex(o, RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(s);
+        }
+
+        internal static bool IsFullMatch(this string s, List<string> o, bool matchCase)
+        {
+            return o.Aggregate(true, (current, i) => current & s.IsMatch(i, matchCase));
+        }
+    }
+}
---
+++ TextSearchApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextSearchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter search items, <press enter for defaults>, Seperate with semicolon");
+            var userItems = Console.ReadLine();
+            var items = userItems?.Split(';').ToList() ?? new List<string> { "Users", "User Groups", "User Activity Log" };
+            Console.WriteLine("Match case? (y/n)");
+            var input = Console.ReadLine();
+            var matchCase = input?.ToUpperInvariant() == "Y" || input?.ToUpperInvariant() == "YES";
+            Console.WriteLine("Items in list are:\r\n");
+            items.ForEach(i => Console.Write(i + ", "));
+            Console.WriteLine("Enter Search String <Press Enter to Continue>:");
+            var s = Console.ReadLine();
+            if (s != null)
+            {
+                var sS = s.Split(' ').ToList();
+                var matches = items.Where(i => i.IsFullMatch(sS, matchCase));
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input terminating program");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
